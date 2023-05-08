7ac72d63a1a073d6a109a70f95a44d6c30291171
jaypoojaramakeen <jay.poojara@makeen.io>
2023-05-08 21:54:29 +0530

    1. Increased timeout.

--- app/src/androidTest/java/com/aws/amazonlocation/ui/main/ExploreFragmentSearchLocationByAddressTest.kt
+++ app/src/androidTest/java/com/aws/amazonlocation/ui/main/ExploreFragmentSearchLocationByAddressTest.kt
@@ -1,12 +1,12 @@
 package com.aws.amazonlocation.ui.main
 
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.* // ktlint-disable no-wildcard-imports
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
@@ -24,8 +24,6 @@ import com.aws.amazonlocation.DELAY_2000
 import com.aws.amazonlocation.R
 import com.aws.amazonlocation.TEST_ADDRESS
 import com.aws.amazonlocation.TEST_FAILED
-import com.aws.amazonlocation.TEST_FAILED_COUNT_NOT_GREATER_THAN_ZERO
-import com.aws.amazonlocation.TEST_FAILED_NO_SEARCH_RESULT
 import com.aws.amazonlocation.di.AppModule
 import com.aws.amazonlocation.enableGPS
 import com.aws.amazonlocation.failTest
@@ -46,7 +44,7 @@ class ExploreFragmentSearchLocationByAddressTest : BaseTest() {
     @get:Rule
     var permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
         ACCESS_FINE_LOCATION,
-        ACCESS_COARSE_LOCATION
+        ACCESS_COARSE_LOCATION,
     )
 
     @get:Rule
@@ -62,22 +60,18 @@ class ExploreFragmentSearchLocationByAddressTest : BaseTest() {
             Thread.sleep(DELAY_2000)
 
             val edtSearch =
-                onView(withId(R.id.edt_search_places)).check(ViewAssertions.matches(isDisplayed()))
+                onView(withId(R.id.edt_search_places)).check(matches(isDisplayed()))
             edtSearch.perform(click())
             onView(withId(R.id.edt_search_places)).perform(typeText(TEST_ADDRESS))
             uiDevice.wait(
                 Until.hasObject(By.res("${BuildConfig.APPLICATION_ID}:id/rv_search_places_suggestion")),
-                DELAY_10000
+                DELAY_10000,
             )
-            val rvSearchPlaceSuggestion =
-                mActivityRule.activity.findViewById<RecyclerView>(R.id.rv_search_places_suggestion)
-            if (rvSearchPlaceSuggestion.adapter?.itemCount != null) {
-                rvSearchPlaceSuggestion.adapter?.itemCount?.let {
-                    Assert.assertTrue(TEST_FAILED_COUNT_NOT_GREATER_THAN_ZERO, it > 0)
-                }
-            } else {
-                Assert.fail(TEST_FAILED_NO_SEARCH_RESULT)
-            }
+            onView(withId(R.id.rv_search_places_suggestion)).check(
+                matches(
+                    hasMinimumChildCount(1),
+                ),
+            )
         } catch (e: Exception) {
             failTest(80, e)
             Assert.fail(TEST_FAILED)
