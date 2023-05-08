8d862740378a94ab3266284b6f90a5edc91b4460
jaypoojaramakeen <jay.poojara@makeen.io>
2023-05-08 11:29:02 +0530

    Changes in map zoom tests and removed adb logcat from workflow.

--- app/src/androidTest/java/com/aws/amazonlocation/ui/main/ExploreFragmentMapZoomInOutTest.kt
+++ app/src/androidTest/java/com/aws/amazonlocation/ui/main/ExploreFragmentMapZoomInOutTest.kt
@@ -3,14 +3,13 @@ package com.aws.amazonlocation.ui.main
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.* // ktlint-disable no-wildcard-imports
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiSelector
-import androidx.test.uiautomator.Until
 import androidx.test.uiautomator.Until.hasObject
 import com.amplifyframework.geo.maplibre.view.MapLibreView
 import com.aws.amazonlocation.ACCESS_COARSE_LOCATION
@@ -24,14 +23,12 @@ import com.aws.amazonlocation.DELAY_2000
 import com.aws.amazonlocation.DELAY_5000
 import com.aws.amazonlocation.R
 import com.aws.amazonlocation.TEST_FAILED
-import com.aws.amazonlocation.TEST_FAILED_LATCH_TIMEOUT
 import com.aws.amazonlocation.TEST_FAILED_MAP_NOT_FOUND
 import com.aws.amazonlocation.TEST_FAILED_ZOOM_LEVEL
 import com.aws.amazonlocation.TEST_FAILED_ZOOM_LEVEL_NOT_CHANGED
 import com.aws.amazonlocation.di.AppModule
 import com.aws.amazonlocation.enableGPS
 import com.aws.amazonlocation.failTest
-import com.aws.amazonlocation.utils.MapCameraZoom
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -39,8 +36,6 @@ import dagger.hilt.android.testing.UninstallModules
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 @UninstallModules(AppModule::class)
 @HiltAndroidTest
@@ -65,14 +60,13 @@ class ExploreFragmentMapZoomInOutTest : BaseTest() {
         try {
             enableGPS(ApplicationProvider.getApplicationContext())
             var mapbox: MapboxMap? = null
-            uiDevice.wait(Until.hasObject(By.desc(AMAZON_MAP_READY)), DELAY_15000)
+            uiDevice.wait(hasObject(By.desc(AMAZON_MAP_READY)), DELAY_15000)
             Thread.sleep(DELAY_1000)
 
             val mapView = mActivityRule.activity.findViewById<MapLibreView>(R.id.mapView)
             mapView.getMapAsync {
                 mapbox = it
             }
-            setDefaultZoom(mapbox)
             val beforeZoomLevel: Double? = mapbox?.cameraPosition?.zoom
             uiDevice.wait(hasObject(By.res("${BuildConfig.APPLICATION_ID}:id/mapView")), DELAY_5000)
             val map = uiDevice.findObject(UiSelector().resourceId("${BuildConfig.APPLICATION_ID}:id/mapView"))
@@ -99,14 +93,13 @@ class ExploreFragmentMapZoomInOutTest : BaseTest() {
     fun testMapZoomOut() {
         try {
             var mapbox: MapboxMap? = null
-            uiDevice.wait(Until.hasObject(By.desc(AMAZON_MAP_READY)), DELAY_15000)
+            uiDevice.wait(hasObject(By.desc(AMAZON_MAP_READY)), DELAY_15000)
             Thread.sleep(DELAY_1000)
 
             val mapView = mActivityRule.activity.findViewById<MapLibreView>(R.id.mapView)
             mapView.getMapAsync {
                 mapbox = it
             }
-            setDefaultZoom(mapbox)
             val beforeZoomLevel: Double? = mapbox?.cameraPosition?.zoom
             uiDevice.wait(hasObject(By.res("${BuildConfig.APPLICATION_ID}:id/mapView")), DELAY_5000)
             val map = uiDevice.findObject(UiSelector().resourceId("${BuildConfig.APPLICATION_ID}:id/mapView"))
@@ -133,14 +126,13 @@ class ExploreFragmentMapZoomInOutTest : BaseTest() {
     fun testMapZoomDoubleTap() {
         try {
             var mapbox: MapboxMap? = null
-            uiDevice.wait(Until.hasObject(By.desc(AMAZON_MAP_READY)), DELAY_15000)
+            uiDevice.wait(hasObject(By.desc(AMAZON_MAP_READY)), DELAY_15000)
             Thread.sleep(DELAY_1000)
 
             val mapView = mActivityRule.activity.findViewById<MapLibreView>(R.id.mapView)
             mapView.getMapAsync {
                 mapbox = it
             }
-            setDefaultZoom(mapbox)
             val beforeZoomLevel: Double? = mapbox?.cameraPosition?.zoom
             uiDevice.wait(hasObject(By.res("${BuildConfig.APPLICATION_ID}:id/mapView")), DELAY_5000)
             val map = uiDevice.findObject(UiSelector().resourceId("${BuildConfig.APPLICATION_ID}:id/mapView"))
@@ -161,20 +153,5 @@ class ExploreFragmentMapZoomInOutTest : BaseTest() {
             failTest(138, e)
             Assert.fail(TEST_FAILED)
         }
-    }
-
-    fun setDefaultZoom(mapboxMap: MapboxMap?) {
-        val latch = CountDownLatch(1)
-
-        getInstrumentation().runOnMainSync {
-            mapboxMap?.getStyle {
-                mapboxMap.moveCamera(com.mapbox.mapboxsdk.camera.CameraUpdateFactory.zoomTo(MapCameraZoom.DEFAULT_CAMERA_ZOOM))
-                latch.countDown()
-            }
-        }
-
-        val success = latch.await(DELAY_5000, TimeUnit.MILLISECONDS)
-
-        Assert.assertTrue(TEST_FAILED_LATCH_TIMEOUT, success)
     }
 }
