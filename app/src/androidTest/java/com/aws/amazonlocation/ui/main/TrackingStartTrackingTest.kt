38eb155fd73769bb89f0dc3a99706577b031889f
jaypoojaramakeen <jay.poojara@makeen.io>
2023-05-10 11:10:44 +0530

    Increased timeout.

--- app/src/androidTest/java/com/aws/amazonlocation/ui/main/TrackingStartTrackingTest.kt
+++ app/src/androidTest/java/com/aws/amazonlocation/ui/main/TrackingStartTrackingTest.kt
@@ -1,9 +1,13 @@
 package com.aws.amazonlocation.ui.main
 
+import android.content.Context
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
@@ -17,6 +21,7 @@ import com.google.android.material.card.MaterialCardView
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.hamcrest.CoreMatchers
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
@@ -31,7 +36,7 @@ class TrackingStartTrackingTest : BaseTest() {
     @get:Rule
     var permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
         ACCESS_FINE_LOCATION,
-        ACCESS_COARSE_LOCATION
+        ACCESS_COARSE_LOCATION,
     )
 
     @get:Rule
@@ -50,7 +55,7 @@ class TrackingStartTrackingTest : BaseTest() {
             tracking.click()
             uiDevice.wait(
                 Until.hasObject(By.text(mActivityRule.activity.getString(R.string.label_enable_tracker_continue_to_tracker))),
-                DELAY_1000
+                DELAY_5000,
             )
             val labelContinue =
                 uiDevice.findObject(By.text(mActivityRule.activity.getString(R.string.label_enable_tracker_continue_to_tracker)))
@@ -58,37 +63,34 @@ class TrackingStartTrackingTest : BaseTest() {
 
             uiDevice.wait(
                 Until.hasObject(By.text(mActivityRule.activity.getString(R.string.label_enable_tracking))),
-                DELAY_1000
+                DELAY_5000,
             )
 
-            val clEnableTracking =
-                mActivityRule.activity.findViewById<ConstraintLayout>(R.id.cl_enable_tracking)
-            if (clEnableTracking.visibility == View.VISIBLE) {
-                val btnEnableTracking =
-                    mActivityRule.activity.findViewById<MaterialCardView>(R.id.btn_enable_tracking)
-                mActivityRule.activity.runOnUiThread {
-                    btnEnableTracking.performClick()
-                }
-            }
+            Espresso.onView(withId(R.id.cl_enable_tracking)).check(matches(isDisplayed()))
+            Espresso.onView(withId(R.id.btn_enable_tracking)).check(matches(isDisplayed())).perform(click())
+
             Thread.sleep(DELAY_5000)
-            val rvTracking =
-                mActivityRule.activity.findViewById<RecyclerView>(R.id.rv_tracking)
+            Thread.sleep(DELAY_3000)
+
             uiDevice.wait(
-                Until.hasObject(By.text(mActivityRule.activity.getString(R.string.label_start_tracking))),
-                DELAY_1000
+                Until.hasObject(By.text(ApplicationProvider.getApplicationContext<Context>().getString(R.string.label_start_tracking))),
+                DELAY_3000,
             )
             val labelStartTracking =
-                uiDevice.findObject(By.text(mActivityRule.activity.getString(R.string.label_start_tracking)))
+                uiDevice.findObject(By.text(ApplicationProvider.getApplicationContext<Context>().getString(R.string.label_start_tracking)))
             labelStartTracking?.click()
 
             Thread.sleep(DELAY_20000)
-            if (rvTracking.adapter?.itemCount != null) {
-                rvTracking.adapter?.itemCount?.let {
-                    Assert.assertTrue(TEST_FAILED_NO_TRACKING_HISTORY, it > 0)
-                }
-            } else {
-                Assert.fail(TEST_FAILED_NO_TRACKING_HISTORY_NULL)
-            }
+            uiDevice.wait(
+                Until.hasObject(By.res(BuildConfig.APPLICATION_ID + ":id/rv_tracking")),
+                DELAY_15000,
+            )
+            Thread.sleep(DELAY_2000)
+            Espresso.onView(CoreMatchers.allOf(withId(R.id.rv_tracking), isDisplayed())).check(
+                matches(
+                    hasMinimumChildCount(1),
+                ),
+            )
         } catch (e: Exception) {
             failTest(93, e)
             Assert.fail(TEST_FAILED)
