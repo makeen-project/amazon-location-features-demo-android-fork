e232993f241fde9c13d62344672b5a136ebf6d31
jaypoojaramakeen <jay.poojara@makeen.io>
2023-05-10 20:40:36 +0530

    Updated timeout.

--- app/src/androidTest/java/com/aws/amazonlocation/ui/main/GeofenceAddTest.kt
+++ app/src/androidTest/java/com/aws/amazonlocation/ui/main/GeofenceAddTest.kt
@@ -20,6 +20,7 @@ import com.aws.amazonlocation.BuildConfig
 import com.aws.amazonlocation.DELAY_1000
 import com.aws.amazonlocation.DELAY_15000
 import com.aws.amazonlocation.DELAY_2000
+import com.aws.amazonlocation.DELAY_3000
 import com.aws.amazonlocation.DELAY_5000
 import com.aws.amazonlocation.R
 import com.aws.amazonlocation.TEST_FAILED
@@ -75,36 +76,21 @@ class GeofenceAddTest : BaseTest() {
 
             uiDevice.wait(
                 Until.gone(By.res("${BuildConfig.APPLICATION_ID}:id/cl_search_loader_geofence_list")),
-                DELAY_5000,
+                DELAY_15000,
             )
             Thread.sleep(DELAY_2000)
             val emptyContainer =
                 mActivityRule.activity.findViewById<View>(R.id.cl_empty_geofence_list)
 
             if (emptyContainer.isVisible) {
-                val btnAddGeofence =
-                    mActivityRule.activity.findViewById<MaterialCardView>(R.id.btn_add_geofence)
-                mActivityRule.activity.runOnUiThread {
-                    btnAddGeofence.performClick()
-                }
+                onView(withId(R.id.btn_add_geofence)).perform(click())
             } else {
-                val cardAddGeofence =
-                    mActivityRule.activity.findViewById<MaterialCardView>(R.id.card_add_geofence)
-                mActivityRule.activity.runOnUiThread {
-                    cardAddGeofence.performClick()
-                }
-            }
-            Thread.sleep(DELAY_2000)
-            val edtAddGeofenceSearch1 =
-                mActivityRule.activity.findViewById<TextInputEditText>(R.id.edt_add_geofence_search)
-            mActivityRule.activity.runOnUiThread {
-                edtAddGeofenceSearch1.performClick()
+                onView(withId(R.id.card_add_geofence)).perform(click())
             }
             Thread.sleep(DELAY_5000)
-            onView(withId(R.id.edt_add_geofence_search)).perform(clearText())
-            onView(withId(R.id.edt_add_geofence_search)).perform(typeText(TEST_WORD_4))
+            onView(withId(R.id.edt_add_geofence_search)).perform(clearText(), typeText(TEST_WORD_4))
 
-            Thread.sleep(DELAY_5000)
+            Thread.sleep(DELAY_15000)
             val rvGeofenceSearchPlaces =
                 mActivityRule.activity.findViewById<RecyclerView>(R.id.rv_geofence_search_places_suggestion)
             rvGeofenceSearchPlaces.adapter?.itemCount?.let {
@@ -117,7 +103,7 @@ class GeofenceAddTest : BaseTest() {
                     )
                 }
             }
-            Thread.sleep(DELAY_2000)
+            Thread.sleep(DELAY_3000)
             val seekbar = mActivityRule.activity.findViewById<SeekBar>(R.id.seekbar_geofence_radius)
             seekbar.progress = 400
 
@@ -131,7 +117,7 @@ class GeofenceAddTest : BaseTest() {
                 btnAddGeofenceSave.performClick()
             }
 
-            uiDevice.wait(Until.hasObject(By.res("${BuildConfig.APPLICATION_ID}:id/rv_geofence")), DELAY_5000)
+            uiDevice.wait(Until.hasObject(By.res("${BuildConfig.APPLICATION_ID}:id/rv_geofence")), DELAY_15000)
 
             onView(withId(R.id.rv_geofence)).perform(
                 RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
