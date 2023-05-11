c13c49d16bbeafd5e88e3bda0af27dcc76f91a13
jaypoojaramakeen <jay.poojara@makeen.io>
2023-05-11 14:00:34 +0530

    Minor change.

--- app/src/androidTest/java/com/aws/amazonlocation/ui/main/ConnectToAWSTest.kt
+++ app/src/androidTest/java/com/aws/amazonlocation/ui/main/ConnectToAWSTest.kt
@@ -1,5 +1,6 @@
 package com.aws.amazonlocation.ui.main
 
+import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
@@ -146,35 +147,45 @@ class ConnectToAWSTest : BaseTest() {
                 DELAY_10000,
             )
             val appViews = UiScrollable(UiSelector().scrollable(true))
-            appViews.scrollForward()
-            Thread.sleep(DELAY_1000)
-            val edtIdentityPoolId =
-                onView(withId(R.id.edt_identity_pool_id)).check(ViewAssertions.matches(isDisplayed()))
-            edtIdentityPoolId.perform(click())
-            Thread.sleep(DELAY_1000)
-            onView(withId(R.id.edt_identity_pool_id))
-                .perform(click(), replaceText(BuildConfig.IDENTITY_POOL_ID), closeSoftKeyboard())
-            Thread.sleep(DELAY_1000)
-            onView(withId(R.id.edt_user_domain))
-                .perform(click(), replaceText(BuildConfig.USER_DOMAIN), closeSoftKeyboard())
-            Thread.sleep(DELAY_1000)
-            onView(withId(R.id.edt_user_pool_client_id))
-                .perform(click(), replaceText(BuildConfig.USER_POOL_CLIENT_ID), closeSoftKeyboard())
-            Thread.sleep(DELAY_1000)
-            onView(withId(R.id.edt_user_pool_id))
-                .perform(click(), replaceText(BuildConfig.USER_POOL_ID), closeSoftKeyboard())
-            Thread.sleep(DELAY_1000)
-            onView(withId(R.id.edt_web_socket_url))
-                .perform(click(), replaceText(BuildConfig.WEB_SOCKET_URL), closeSoftKeyboard())
 
-            Thread.sleep(DELAY_1000)
+            appViews.scrollIntoView(UiSelector().resourceId("${BuildConfig.APPLICATION_ID}:id/tv_user_domain"))
+            Thread.sleep(DELAY_2000)
+            val edtIdentityPoolId = onView(withId(R.id.edt_identity_pool_id)).check(ViewAssertions.matches(isDisplayed()))
+            edtIdentityPoolId.perform(click())
+            Thread.sleep(DELAY_2000)
+            onView(withId(R.id.edt_identity_pool_id)).perform(click(), replaceText(BuildConfig.IDENTITY_POOL_ID))
+            appViews.scrollIntoView(UiSelector().resourceId("${BuildConfig.APPLICATION_ID}:id/tv_user_pool_client_id"))
+            Thread.sleep(DELAY_2000)
+            onView(withId(R.id.edt_user_domain)).perform(click(), replaceText(BuildConfig.USER_DOMAIN))
+            appViews.scrollIntoView(UiSelector().resourceId("${BuildConfig.APPLICATION_ID}:id/tv_user_pool_id"))
+            Thread.sleep(DELAY_2000)
+            onView(withId(R.id.edt_user_pool_client_id)).perform(click(), replaceText(BuildConfig.USER_POOL_CLIENT_ID))
+            appViews.scrollIntoView(UiSelector().resourceId("${BuildConfig.APPLICATION_ID}:id/tv_web_socket_url"))
+            Thread.sleep(DELAY_2000)
+            onView(withId(R.id.edt_user_pool_id)).perform(click(), replaceText(BuildConfig.USER_POOL_ID))
+            appViews.scrollForward()
+            Thread.sleep(DELAY_2000)
+            onView(withId(R.id.edt_web_socket_url)).perform(click(), replaceText(BuildConfig.WEB_SOCKET_URL))
+            Thread.sleep(DELAY_2000)
             val btnConnect =
                 onView(withId(R.id.btn_connect)).check(ViewAssertions.matches(isDisplayed()))
             btnConnect.perform(click())
             Thread.sleep(DELAY_5000)
+
             val targetContext: Context = getInstrumentation().targetContext.applicationContext
             val pm = PreferenceManager(targetContext)
             val mPoolId = pm.getValue(KEY_POOL_ID, "")
+
+            val packageName = targetContext.packageName
+            // Clear app from recent apps list
+            val am = targetContext.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+            am.let {
+                it.appTasks.forEach { task ->
+                    if (task.taskInfo.baseActivity?.packageName == packageName) {
+                        task.finishAndRemoveTask()
+                    }
+                }
+            }
             Assert.assertTrue(TEST_FAILED_INVALID_IDENTITY_POOL_ID, mPoolId == BuildConfig.IDENTITY_POOL_ID)
         } catch (e: Exception) {
             Assert.fail(TEST_FAILED_CONNECT_TO_AWS_FROM_TRACKING)
