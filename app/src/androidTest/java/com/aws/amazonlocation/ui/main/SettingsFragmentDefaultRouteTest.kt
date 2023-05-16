eca23e977ab9b4fdc5d19ccf5acfcbb1e2555bbe
jaypoojaramakeen <jay.poojara@makeen.io>
2023-05-16 21:24:48 +0530

    Updated more test classes to use espresso.

--- app/src/androidTest/java/com/aws/amazonlocation/ui/main/SettingsFragmentDefaultRouteTest.kt
+++ app/src/androidTest/java/com/aws/amazonlocation/ui/main/SettingsFragmentDefaultRouteTest.kt
@@ -1,34 +1,27 @@
 package com.aws.amazonlocation.ui.main
 
-import android.view.View
 import androidx.annotation.IdRes
 import androidx.appcompat.widget.SwitchCompat
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.* // ktlint-disable no-wildcard-imports
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import androidx.test.rule.ActivityTestRule
-import androidx.test.rule.GrantPermissionRule
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
-import com.aws.amazonlocation.*
+import com.aws.amazonlocation.* // ktlint-disable no-wildcard-imports
 import com.aws.amazonlocation.di.AppModule
-import com.aws.amazonlocation.utils.*
-import com.google.android.material.card.MaterialCardView
-import com.google.android.material.textfield.TextInputEditText
-import dagger.hilt.android.testing.HiltAndroidRule
+import com.aws.amazonlocation.utils.* // ktlint-disable no-wildcard-imports
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.hamcrest.CoreMatchers
 import org.hamcrest.core.AllOf.allOf
-import org.junit.*
+import org.junit.* // ktlint-disable no-wildcard-imports
 
 @UninstallModules(AppModule::class)
 @HiltAndroidTest
@@ -113,31 +106,32 @@ class SettingsFragmentDefaultRouteTest : BaseTestMainActivity() {
     }
 
     private fun goToDefaultRouteSettings() {
+        waitForView(
+            allOf(
+                withId(R.id.bottom_navigation_main),
+                isDisplayed(),
+            ),
+        )
+
         val explorer =
             uiDevice.findObject(By.text(mActivityRule.activity.getString(R.string.menu_setting)))
         explorer.click()
 
-        uiDevice.wait(
-            Until.hasObject(By.res("${BuildConfig.APPLICATION_ID}:id/cl_route_option")),
-            DELAY_10000,
+        val routeOptions = waitForView(
+            allOf(
+                withId(R.id.cl_route_option),
+                isDisplayed(),
+            ),
         )
-        Thread.sleep(DELAY_2000)
-
-        val clRouteOption =
-            mActivityRule.activity.findViewById<ConstraintLayout>(R.id.cl_route_option)
-        if (clRouteOption.visibility == View.VISIBLE) {
-            mActivityRule.activity.runOnUiThread {
-                clRouteOption.performClick()
-            }
-        }
+        routeOptions?.perform(click())
     }
 
     private fun toggleSwitch(@IdRes switchId: Int) {
         onView(
             allOf(
                 withId(switchId),
-                isDisplayed()
-            )
+                isDisplayed(),
+            ),
         ).perform(click())
     }
 
@@ -149,69 +143,81 @@ class SettingsFragmentDefaultRouteTest : BaseTestMainActivity() {
         uiDevice.wait(Until.hasObject(By.desc(AMAZON_MAP_READY)), DELAY_15000)
         Thread.sleep(DELAY_2000)
 
-        val cardDirection =
-            mActivityRule.activity.findViewById<MaterialCardView>(R.id.card_direction)
-        if (cardDirection.visibility == View.VISIBLE) {
-            val cardDirectionTest =
-                onView(withId(R.id.card_direction)).check(ViewAssertions.matches(isDisplayed()))
-            cardDirectionTest.perform(click())
-            Thread.sleep(DELAY_2000)
-            uiDevice.wait(
-                Until.hasObject(By.res(BuildConfig.APPLICATION_ID + ":id/edt_search_direction")),
-                DELAY_5000
-            )
-            val edtSearchDirection =
-                mActivityRule.activity.findViewById<TextInputEditText>(R.id.edt_search_direction)
-            if (edtSearchDirection.visibility == View.VISIBLE) {
-                typeTextAndSelectSuggestion(R.id.edt_search_direction, SEARCH_TEST_WORD_1)
-                typeTextAndSelectSuggestion(R.id.edt_search_dest, SEARCH_TEST_WORD_2)
-                Thread.sleep(DELAY_2000)
-                uiDevice.wait(
-                    Until.hasObject(By.res(BuildConfig.APPLICATION_ID + ":id/card_routing_option")),
-                    DELAY_10000
-                )
+        val cardDirectionTest =
+            onView(withId(R.id.card_direction)).check(ViewAssertions.matches(isDisplayed()))
+        cardDirectionTest.perform(click())
 
-                val optionsContainer = mActivityRule.activity.findViewById<MaterialCardView>(R.id.card_list_routes_option)
-
-                if (!optionsContainer.isVisible) {
-                    onView(withId(R.id.card_routing_option)).perform(click())
-                }
-                Thread.sleep(DELAY_2000)
-                val switchAvoidToll = mActivityRule.activity.findViewById<SwitchCompat>(R.id.switch_avoid_tools)
-                val switchAvoidFerry = mActivityRule.activity.findViewById<SwitchCompat>(R.id.switch_avoid_ferries)
-
-                Thread.sleep(DELAY_1000)
-
-                if (switchAvoidToll.isChecked != avoidTollsShouldBe || switchAvoidFerry.isChecked != avoidFerriesShouldBe) {
-                    Assert.fail(TEST_FAILED_DEFAULT_ROUTE_OPTIONS_NOT_LOADED)
-                }
-            } else {
-                Assert.fail(TEST_FAILED_SEARCH_DIRECTION)
-            }
-        } else {
-            Assert.fail(TEST_FAILED_DIRECTION_CARD)
-        }
-    }
-
-    private fun typeTextAndSelectSuggestion(@IdRes viewId: Int, text: String) {
-        onView(withId(viewId)).perform(ViewActions.replaceText(text))
         Thread.sleep(DELAY_2000)
-        uiDevice.wait(
-            Until.hasObject(By.res(BuildConfig.APPLICATION_ID + ":id/rv_search_places_suggestion_direction")),
-            DELAY_10000
+
+        val sourceEdt = waitForView(CoreMatchers.allOf(withId(R.id.edt_search_direction), isDisplayed()))
+        sourceEdt?.perform(replaceText(SEARCH_TEST_WORD_1))
+
+        Thread.sleep(DELAY_2000)
+
+        val suggestionListSrcRv = waitForView(
+            CoreMatchers.allOf(
+                withId(R.id.rv_search_places_suggestion_direction),
+                isDisplayed(),
+                hasMinimumChildCount(1),
+            ),
         )
-        val rvSearchPlacesSuggestionDirection =
-            mActivityRule.activity.findViewById<RecyclerView>(R.id.rv_search_places_suggestion_direction)
-        rvSearchPlacesSuggestionDirection.adapter?.itemCount?.let {
-            if (it > 0) {
-                onView(withId(R.id.rv_search_places_suggestion_direction)).perform(
-                    RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                        0,
-                        click()
-                    )
-                )
+        suggestionListSrcRv?.perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click(),
+            ),
+        )
+
+        Thread.sleep(DELAY_2000)
+
+        val destinationEdt = waitForView(
+            CoreMatchers.allOf(
+                withId(R.id.edt_search_dest),
+                isDisplayed(),
+            ),
+        )
+        destinationEdt?.perform(replaceText(SEARCH_TEST_WORD_2))
+
+        Thread.sleep(DELAY_2000)
+
+        val suggestionListDestRv = waitForView(
+            CoreMatchers.allOf(
+                withId(R.id.rv_search_places_suggestion_direction),
+                isDisplayed(),
+                hasMinimumChildCount(1),
+            ),
+        )
+        suggestionListDestRv?.perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click(),
+            ),
+        )
+
+        waitForView(
+            CoreMatchers.allOf(
+                withId(R.id.card_drive_go),
+                isDisplayed(),
+            ),
+        )
+
+        val cardRoutingOption = waitForView(
+            CoreMatchers.allOf(
+                withId(R.id.card_routing_option),
+                withEffectiveVisibility(Visibility.VISIBLE),
+            ),
+        )
+        cardRoutingOption?.perform(click())
+
+        Thread.sleep(DELAY_2000)
+
+        getInstrumentation().runOnMainSync {
+            val switchAvoidToll = mActivityRule.activity.findViewById<SwitchCompat>(R.id.switch_avoid_tools)
+            val switchAvoidFerry = mActivityRule.activity.findViewById<SwitchCompat>(R.id.switch_avoid_ferries)
+
+            if (switchAvoidToll.isChecked != avoidTollsShouldBe || switchAvoidFerry.isChecked != avoidFerriesShouldBe) {
+                Assert.fail(TEST_FAILED_DEFAULT_ROUTE_OPTIONS_NOT_LOADED)
             }
         }
     }
-
 }
